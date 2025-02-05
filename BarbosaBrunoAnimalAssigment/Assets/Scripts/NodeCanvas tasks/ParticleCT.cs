8b4c06bed110d0053ba874a9337d588a84915c6b
dg015 <bruno.barbosa8@yahoo.com>
2025-02-05 17:24:59 -0500

    crate first signifier

---
+++ BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/ParticleCT.cs
@@ -0,0 +1,35 @@
+using NodeCanvas.Framework;
+using NodeCanvas.Tasks.Actions;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Conditions {
+
+	public class ParticleCT : ConditionTask {
+		public GameObject playIcon;
+		public Transform spawnPoint;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit(){
+			return null;
+		}
+
+		//Called whenever the condition gets enabled.
+		protected override void OnEnable() {
+			GameObject Particle = Object.Instantiate(playIcon, spawnPoint);
+
+        }
+
+		//Called whenever the condition gets disabled.
+		protected override void OnDisable() {
+			
+		}
+
+		//Called once per frame while the condition is active.
+		//Return whether the condition is success or failure.
+		protected override bool OnCheck() {
+			return true;
+		}
+	}
+}
