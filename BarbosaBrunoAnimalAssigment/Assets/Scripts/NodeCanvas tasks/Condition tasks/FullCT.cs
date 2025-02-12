c04793cf5fea9cd732eb43576caab3f14d43fe5d
dg015 <bruno.barbosa8@yahoo.com>
2025-02-11 23:35:53 -0500

    finished is full script

--- BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Condition tasks/FullCT.cs
+++ BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Condition tasks/FullCT.cs
@@ -1,14 +1,17 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-
+using UnityEngine;
 
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class FullCT : ConditionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit(){
+        public BBParameter<float> hunger;
+        public float maxHunger;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit(){
 			return null;
 		}
 
@@ -25,7 +28,15 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			if (hunger.value >= maxHunger)
+			{
+                return true;
+            }
+			else
+			{
+				return false ;
+            }
+			
 		}
 	}
 }
--- BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Condition tasks/RestedCT.cs
+++ BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Condition tasks/RestedCT.cs
@@ -28,7 +28,7 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			if(sleep.value == maxSleep)
+			if(sleep.value >= maxSleep)
 			{
                 return true;
             }
