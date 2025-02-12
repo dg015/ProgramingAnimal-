a7a2d64586ea45bec47904acc6f31e1642f01e72
dg015 <bruno.barbosa8@yahoo.com>
2025-02-11 23:13:21 -0500

    fixed random state generator

--- BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Action Tasks/ThinkingAT.cs
+++ BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Action Tasks/ThinkingAT.cs
@@ -1,10 +1,13 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ThinkingAT : ActionTask {
+
+		public BBParameter<int> StateID;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -16,22 +19,30 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+			generateNewEvent();
+			//EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
 
-		//Called when the task is disabled.
-		protected override void OnStop() {
+        }
+
+        //Called when the task is disabled.
+        protected override void OnStop() {
 			
 		}
 
 		//Called when the task is paused.
 		protected override void OnPause() {
 			
+		}
+
+		private void generateNewEvent()
+		{
+			StateID.value = Random.Range(2,5);
+			Debug.Log(StateID.value.ToString());
+
 		}
 	}
 }
