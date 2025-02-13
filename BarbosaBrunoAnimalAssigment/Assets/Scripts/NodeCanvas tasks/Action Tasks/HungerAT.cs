ae265849c827fe66c7f4eec649ba6fd92fd80136
dg015 <bruno.barbosa8@yahoo.com>
2025-02-13 15:43:03 -0500

    Hunger and sleep drain created

---
+++ BarbosaBrunoAnimalAssigment/Assets/Scripts/NodeCanvas tasks/Action Tasks/HungerAT.cs
@@ -0,0 +1,48 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class HungerAT : ActionTask {
+		public BBParameter<float> hunger;
+        public BBParameter<bool> isEating;
+		public float hungerMin;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+			
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+            if (hunger.value > hungerMin && !isEating.value)
+            {
+                hunger.value -= Time.deltaTime;
+            }
+            else if (isEating.value)
+            {
+                Debug.Log("eating");
+            }
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
